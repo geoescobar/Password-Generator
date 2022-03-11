cea5a65736c356c89a523fb5c86505bc92c51620
George Escobar <geo.escobar214@gmail.com>
2022-03-11 13:38:53 -0600

    homework

---
+++ README.md
@@ -0,0 +1,9 @@
+# 03 JavaScript: Password Generator
+
+Welcome to my password generator! 
+
+I began by adding variables with arrays containing the characters that will be apart of the password if selected. 
+
+Next, within the generate password function I included the code for the actual password. I used confirm to decide wether or not to use specific characters. At the end it will populate the password based on the selected characters. For the length of characters was the only time I used prompt.
+
+Once all the actions to decide the length and characters of the password I linked them all into the write password fuction that will display it on the page.
---
+++ assets/script.js
@@ -0,0 +1,63 @@
+var generateBtn = document.querySelector("#generate");
+
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '>', '<', ')', '('];
+
+var allowedCharacters = [];
+
+
+function generatePassword() {
+
+ 
+  var confirmNumberOfCharacters = prompt('Input number of password characters:');
+
+  var confirmUppercase = confirm('Do you want to include uppercase letters?');
+  if (confirmUppercase === true) {
+    for (var i = 0; i < uppercase.length; i++) {
+      allowedCharacters.push(uppercase[i]);
+    }
+  };
+
+  var confirmLowercaseLetters = confirm('Do you want to include lowercase letters? Input yes or no:');
+  if (confirmLowercaseLetters === true) {
+    for (var i = 0; i < lowercase.length; i++) {
+      allowedCharacters.push(lowercase[i]);
+    }
+  };
+  
+  var confirmNumbers = confirm('Do you want to include number? Input yes or no:');
+  if (confirmNumbers === true) {
+    for (var i = 0; i < numbers.length; i++) {
+      allowedCharacters.push(numbers[i]);
+    }
+  };
+
+  var confirmSymbols = confirm('Do you want to include symbols? Input yes or no:');
+  if (confirmSymbols === true) {
+    for (var i = 0; i < symbols.length; i++) {
+      allowedCharacters.push(symbols[i]);
+    }
+  };
+  
+  var password = '';
+  for (i = 0; i < confirmNumberOfCharacters; i++) {
+    var randomIndex = Math.floor(Math.random() * allowedCharacters.length);
+    var randomChar = allowedCharacters[randomIndex];
+    password = password + randomChar;
+  }
+
+  return password;
+}
+
+function writePassword() {
+  var passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
+
